433d042678f71d072d85c84e0332c2bdea157da1
pechischev_vladimir <https://github.com/pechischev>
2019-07-02 22:42:49 +0300

    append support yandex map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {YMaps, Map} from 'react-yandex-maps';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App: React.FC = () => {
+    return (
+        <div className="App" style={{height: "100vh"}}>
+            <YMaps>
+                <Map
+                    defaultState={{center: [56.630842, 47.886089], zoom: 16, conrols: []}}
+                    style={{width: "100%", height: "100%"}}
+                />
+            </YMaps>
+        </div>
+    );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+import {Component} from "react";
+
+export type TNullableTypes = Component | HTMLElement;
+
+export type Nullable<T> = T extends (null | TNullableTypes) ? (T | null | undefined) : (T | undefined);
